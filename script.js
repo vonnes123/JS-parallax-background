169c6c85205a8615135cd33f5dae373f4f1b5581
Vasil <vasil.todorov@student.fontys.nl>
2022-10-14 12:52:49 +0200

    add 5 layer animation via class

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
-let gameSpeed = 15;
+let gameSpeed = 5;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = "layer-1.png";
@@ -15,17 +15,49 @@ backgroundLayer4.src = "layer-4.png";
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = "layer-5.png";
 
-let x = 0;
-let x2 = 2400;
+class Layer {
+    constructor(image, speedModifier){
+        this.x = 0;
+        this.y = 0;
+        this.width = 2400;
+        this.height = 700;
+        this.x2 = this.width;
+        this.image = image;
+        this.speedModifier = speedModifier;
+        this.speed = gameSpeed * this.speedModifier;
+    }
+    update(){
+        this.speed = gameSpeed * this.speedModifier;
+        if (this.x <= -this.width){
+            this.x = this.width + this.x2 - this.speed;
+        }
+        if( this.x2 <= -this.width){
+            this.x2 = this.width + this.x - this.speed;
+        }
+        this.x = Math.floor(this.x - this.speed);
+        this.x2 = Math.floor(this.x2 - this.speed);
+    }
+    draw(){
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
+    }
+}
+
+const layer1 = new Layer(backgroundLayer1, 0.2);
+const layer2 = new Layer(backgroundLayer2, 0.4);
+const layer3 = new Layer(backgroundLayer3, 0.6);
+const layer4 = new Layer(backgroundLayer4, 0.8);
+const layer5 = new Layer(backgroundLayer5, 1);
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5]
+
 
 function animate(){
     ctx.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT)
-    ctx.drawImage(backgroundLayer4, x, 0);
-    ctx.drawImage(backgroundLayer4, x2, 0);
-    if(x < -2400) x = 2400 - gameSpeed;
-    else x -= gameSpeed;
-    if (x2 < -2400) x2 = 2400 + x - gameSpeed;
-    else x2 -= gameSpeed;
+    gameObjects.forEach(object => {
+        object.update();
+        object.draw();
+    })
     requestAnimationFrame(animate);
 }
 animate();
